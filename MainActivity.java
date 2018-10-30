4c670fc70bbd051fa43ba2f71e04d0ce8c944045
Freddy Kouakou <41835774+FreddyKouakou@users.noreply.github.com>
2018-10-30 23:05:16 +0000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package team19.fredmichpaul.e_lessonplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
